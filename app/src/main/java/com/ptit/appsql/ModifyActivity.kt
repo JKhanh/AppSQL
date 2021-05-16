5474ce30e25e08676f2f892511e5bb7e22ab5467
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2021-05-16 19:37:54 +0700

    Fix fragment in activity

--- app/src/main/java/com/ptit/appsql/ModifyActivity.kt
+++ app/src/main/java/com/ptit/appsql/ModifyActivity.kt
@@ -2,14 +2,10 @@ package com.ptit.appsql
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.ptit.appsql.databinding.ActivityModifyBinding
 
 class ModifyActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityModifyBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityModifyBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_modify)
     }
 }
